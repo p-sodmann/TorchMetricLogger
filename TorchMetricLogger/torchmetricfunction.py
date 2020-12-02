c7ef8d73f4a764d9bb1724955237438f465d9776
Phil <psodmann@gmail.com>
2020-12-02 09:34:53 +0100

    added axes to tml

--- TorchMetricLogger/torchmetricfunction.py
+++ TorchMetricLogger/torchmetricfunction.py
@@ -20,3 +20,16 @@ class TMLBinaryAccuracy(TmlMetricFunction):
         
     def calculate(self, gold_labels, predictions):
         return (gold_labels > 0.5) == (predictions > 0.5)
+
+# testing axes at the moment
+class TMLDiceCoefficient(TmlMetricFunction):
+    def __init__(self, axis=-1):
+        super().__init__()
+        self.axis=axis
+        
+    def calculate(self, gold_labels, predictions):
+        smooth = 1
+        
+        intersection = torch.sum(gold_labels * predictions, axis=self.axis)
+        score = (2. * intersection + smooth) / (torch.sum(gold_labels, axis=self.axis) + torch.sum(predictions, axis=self.axis) + smooth)
+        return score
